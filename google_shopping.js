b30da70fbf133d79b9ae8212aa74fd0857505fea
Paul <pacalabre@gmail.com>
2016-03-23 18:13:54 -0700

    added solutions up to question 5

--- google_shopping.js
+++ google_shopping.js
@@ -40,3 +40,37 @@ console.log(count);*/
 //       console.log(images);
 //   }
 // }
+
+
+
+
+//Question 4
+/*
+4.) Find all canon products in the items (careful with case sensitivity).
+*/
+
+// for(var i=0; i < data['items'].length; i++) {
+//   var brand = data['items'][i].product.brand;
+//   //console.log(brand);
+//   if(brand === 'Canon') {
+//     console.log(brand);
+//  }
+// }
+
+
+
+
+//Question 5
+/*
+5.) Find all items that have product author name of "eBay" and are brand "Canon".
+*/
+
+  for(i=0; i<data['items'].length; i++) {
+    var product = data['items'][i].product;
+    var author = console.log(data['items'][i].product.author.name[0].indexOf('ebay'));
+    var brand = data['items'][i].product.brand;
+     //console.log(author);
+    if (brand==="Canon" && data['items'][i].product.author.name[0].indexOf('ebay')) {
+      console.log(data['items'][i].product);
+    }
+  }
